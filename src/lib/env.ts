59d008db58744f637b1024f625eb350fbf03d534
Kibryant <arthurgustavon@gmail.com>
2024-08-08 15:14:08 -0300

    apply clean arch

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,9 +4,9 @@ import { config } from "dotenv";
 config();
 
 const envSchema = z.object({
-  MONGODB_URI: z.string(),
-  JWT_SECRET_KEY: z.string(),
-  HOTMART_HOTTOK: z.string(),
+    MONGODB_URI: z.string(),
+    JWT_SECRET_KEY: z.string(),
+    HOTMART_HOTTOK: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
